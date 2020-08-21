36f8f54e7a6e38e6f15ade2c15646ef870a195a5
Nigel Bishop <nbishop18@gmail.com>
2020-08-20 22:41:37 -0400

    Eureka server added

--- BankAccounts/src/main/java/com/bank/BankAccountsApplication.java
+++ BankAccounts/src/main/java/com/bank/BankAccountsApplication.java
@@ -2,12 +2,15 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BankAccountsApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BankAccountsApplication.class, args);
 	}
-
+	
 }
--- BankCustomer/src/main/java/com/bank/BankCustomerApplication.java
+++ BankCustomer/src/main/java/com/bank/BankCustomerApplication.java
@@ -2,9 +2,11 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BankCustomerApplication {
 
 	public static void main(String[] args) {
